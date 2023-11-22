e7874dd5b396447f52b6a2f4659f5790fd3e1179
Ore James <jamesore200@gmail.com>
2023-11-22 03:12:24 -0500

    Fixed dependencies

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ import os
 load_dotenv()
 
 
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+
 
 app = Flask(__name__)
 
@@ -174,4 +173,6 @@ def add_recommendation_page():
 
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     app.run(debug=True)
